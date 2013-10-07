e4ef2c728d16f02991b590d385e5dbe0840da24f
Mohamed Baig <mohamed.b.baig@gmail.com>
2013-10-06 22:26:28 -0400

    Changed the name of the extension to make more sense

--- bootstrap.js
+++ bootstrap.js
@@ -11,22 +11,22 @@ Cu.import("resource:///modules/devtools/gDevTools.jsm");
 
 XPCOMUtils.defineLazyGetter(this, "osString", () => Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime).OS);
 
-XPCOMUtils.defineLazyGetter(this, "toolStrings", () => Services.strings.createBundle("chrome://my-addon/locale/strings.properties"));
+XPCOMUtils.defineLazyGetter(this, "toolStrings", () => Services.strings.createBundle("chrome://publisher/locale/strings.properties"));
 
 XPCOMUtils.defineLazyGetter(this, "toolDefinition", () => ({
-  id: "my-addon",
+  id: "publisher",
   ordinal: 99,
-  key: toolStrings.GetStringFromName("MyAddon.commandkey"),
+  key: toolStrings.GetStringFromName("Publisher.commandkey"),
   modifiers: osString == "Darwin" ? "accel,alt" : "accel,shift",
-  icon: "chrome://my-addon/skin/icon.png",
-  url: "chrome://my-addon/content/tool.xul",
-  label: toolStrings.GetStringFromName("MyAddon.label"),
-  tooltip: toolStrings.GetStringFromName("MyAddon.tooltip"),
+  icon: "chrome://publisher/skin/icon.png",
+  url: "chrome://publisher/content/tool.xul",
+  label: toolStrings.GetStringFromName("Publisher.label"),
+  tooltip: toolStrings.GetStringFromName("Publisher.tooltip"),
   isTargetSupported: function(target) {
     return target.isLocalTab;
   },
   build: function(iframeWindow, toolbox) {
-    Cu.import("chrome://my-addon/content/panel.js");
+    Cu.import("chrome://publisher/content/panel.js");
     let panel = new MyAddonPanel(iframeWindow, toolbox);
     return panel.open();
   }
